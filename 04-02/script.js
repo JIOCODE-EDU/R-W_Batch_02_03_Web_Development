ae4d3213bde0af02e8383cbc1668e68d92166c50
jiocode-edu <ifmdac80@gmail.com>
2026-02-05 16:37:14 +0530

    05-02-2026

---
+++ 04-02/script.js
@@ -0,0 +1,109 @@
+// // Javascript Map and Set Object
+
+// // Map Object
+
+// let obj = {
+//   name:"alice",
+//   age:20
+// }
+
+// obj.gender = "male"
+
+// console.log(obj);
+
+// let map = new Map()
+
+// // map["key1"]= "toys"
+// // map.key2 = "instruments"
+
+
+// // set method
+
+// map.set("key1" , "toys")
+// map.set("key2" , "instruments")
+// map.set("key3" , "fruits")
+// map.set("key4" , "vegitables")
+
+// // has method
+
+// console.log(map.size);
+// console.log(map.has("key4"));
+// // console.log(map.delete("key3"));
+// // console.log(map.clear());
+
+// console.log(map);
+
+// // Map entries
+
+// // let Mapentries = map.entries()
+// // let Mapentries = map.keys()
+// let Mapentries = map.values()
+
+// console.log(Mapentries.next().value);
+// console.log(Mapentries.next().value);
+// console.log(Mapentries.next().value);
+// console.log(Mapentries.next().value);
+// console.log(Mapentries.next().value);
+// // console.log(Mapentries.next().value);
+// // console.log(Mapentries.next().value);
+// // console.log(Mapentries.next().value);
+
+// // Map forEach
+
+
+// let result = map.forEach(PrintMap)
+
+// function PrintMap(value , key , map){
+//   console.log(`${key}:${value}`);
+// }
+
+
+// Set Object
+
+const set = new Set()
+
+
+set.add("Alice")
+set.add("Bob")
+set.add("Charlie")
+set.add("David")
+
+console.log(set);
+
+// console.log(set.has("Charlie"));
+// console.log(set.has("john"));
+
+// let setEntries = set.entries()
+// let setEntries = set.keys()
+// let setEntries = set.values()
+
+// console.log(setEntries.next().value);
+// console.log(setEntries.next().value);
+// console.log(setEntries.next().value);
+// console.log(setEntries.next().value);
+
+// set.clear()
+
+// set.delete("Charlie")
+
+// console.log(set);
+
+// set.forEach(setPrint)
+
+// function setPrint(value , key , set){
+//   console.log(`${key} : ${value}`);
+// }
+
+// console.log(set.size);
+
+
+
+
+
+
+
+
+
+
+
+
