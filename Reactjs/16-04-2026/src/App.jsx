6676632cfa12900392ba1146a18804ee312c7d9d
jiocode-edu <ifmdac80@gmail.com>
2026-04-16 15:32:17 +0530

    16-04-2026

---
+++ Reactjs/16-04-2026/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Layout from './pages/Layout'
+import SimpleSlider from './components/Slider'
+
+function App() {
+
+  return (
+    <>
+      <Layout/>
+      <SimpleSlider/>
+    </>
+  )
+}
+
+export default App
